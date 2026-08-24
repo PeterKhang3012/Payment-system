7d3ee6ff0108566f1e03351db4ebce34552a545d
PeterKhang3012 <9conso@gmail.com>
2026-08-24 20:46:13 +0700

    feat: implement login feature

--- auth-service/src/routes/auth.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { loginController } from "../controller/auth.controller.js";
-
-// login
-router.post("/login", loginController);
-
-export default router;
